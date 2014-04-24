4376b2b2f7c0de26d7dd6776f4c9ac8f61554085
PeterTheOne <petertheone@gmail.com>
2014-04-24 21:42:15 +0200

    smooth movement inputs

--- js/script.js
+++ js/script.js
@@ -12,6 +12,17 @@ var terrainSimulation = function() {
         x: 0,
         y: 0
     };
+    var newMousedownPosition = {
+        x: 0,
+        y: 0
+    };
+
+    var moveForwardsKeyDown = false;
+    var moveBackwardsKeyDown = false;
+    var moveLeftKeyDown = false;
+    var moveRightKeyDown = false;
+
+    var oldTimestamp = 0;
 
     var init = function() {
         scene = new THREE.Scene();
@@ -29,6 +40,7 @@ var terrainSimulation = function() {
         renderer.domElement.setAttribute('tabindex', 1);
         renderer.domElement.focus();
         renderer.domElement.addEventListener('keydown', keydown, false);
+        renderer.domElement.addEventListener('keyup', keyup, false);
         renderer.domElement.addEventListener('mousedown', mousedown, false);
         renderer.domElement.addEventListener('mouseup', mouseup, false);
         renderer.domElement.addEventListener('mousemove', mousemove, false);
@@ -49,53 +61,104 @@ var terrainSimulation = function() {
 
     var keydown = function(event) {
         if (event.keyCode === 87) { // w
-            camera.translateZ(-1);
+            moveForwardsKeyDown = true;
         } else if (event.keyCode === 83) { // s
-            camera.translateZ(1);
+            moveBackwardsKeyDown = true;
         } else if (event.keyCode === 65) { // a
-            camera.translateX(-1);
+            moveLeftKeyDown = true;
         } else if (event.keyCode === 68) { // d
-            camera.translateX(1);
+            moveRightKeyDown = true;
+        }
+    };
+
+    var keyup = function(event) {
+        if (event.keyCode === 87) { // w
+            moveForwardsKeyDown = false;
+        } else if (event.keyCode === 83) { // s
+            moveBackwardsKeyDown = false;
+        } else if (event.keyCode === 65) { // a
+            moveLeftKeyDown = false;
+        } else if (event.keyCode === 68) { // d
+            moveRightKeyDown = false;
         }
     };
 
     var mousedown = function(event) {
-        if (event.keyCode === 0) { // first mouse button
+        var keyCode = event.keyCode || event.which - 1;
+        if (keyCode === 0) { // first mouse button
             isMousedown = true;
-            mousedownPosition.x = event.offsetX / window.innerWidth;
-            mousedownPosition.y = event.offsetY / window.innerHeight;
+            // todo: maybe use offset, but offset doesn't work on FF.
+            mousedownPosition.x = event.pageX / window.innerWidth;
+            mousedownPosition.y = event.pageY / window.innerHeight;
+            newMousedownPosition = mousedownPosition;
         }
     };
 
     var mouseup = function(event) {
-        if (event.keyCode === 0) { // first mouse button
+      var keyCode = event.keyCode || event.which - 1;
+        if (keyCode === 0) { // first mouse button
             isMousedown = false;
         }
     };
 
     var mousemove = function(event) {
         if (isMousedown) {
-            var newMousedownPosition = {
-                x: event.offsetX / window.innerWidth,
-                y: event.offsetY / window.innerHeight
+            // todo: maybe use offset, but offset doesn't work on FF.
+            newMousedownPosition = {
+                x: event.pageX / window.innerWidth,
+                y: event.pageY / window.innerHeight
             };
+        }
+    };
+
+    var gameloop = function(timestamp) {
+        requestAnimationFrame(gameloop);
+
+        var deltaTime = 0;
+        if (typeof timestamp != 'undefined') {
+            if (oldTimestamp !== 0) {
+                deltaTime = timestamp - oldTimestamp;
+            }
+            oldTimestamp = timestamp;
+        }
+
+        update(deltaTime);
+        render();
+    };
+
+    var update = function(deltaTime) {
+        //console.log(1000 / deltaTime);
+
+        // input update
+        // mouse
+        if (isMousedown) {
             var mouseDiff = {
                 x: newMousedownPosition.x - mousedownPosition.x,
                 y: newMousedownPosition.y - mousedownPosition.y
             };
-            camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), -mouseDiff.x * 10);
-            camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), -mouseDiff.y * 10);
+            var mouseSensitivity = {
+                x: 0.5,
+                y: 0.5
+            };
+            camera.rotateOnAxis(new THREE.Vector3(0, -1, 0), mouseDiff.x * deltaTime * mouseSensitivity.x);
+            camera.rotateOnAxis(new THREE.Vector3(-1, 0, 0), mouseDiff.y * deltaTime * mouseSensitivity.y);
             mousedownPosition = newMousedownPosition;
         }
-    };
+        // keyboard
+        var movementSpeed = 0.05;
+        if (moveForwardsKeyDown) {
+            camera.translateZ(-movementSpeed * deltaTime);
+        }
+        if (moveBackwardsKeyDown) {
+            camera.translateZ(movementSpeed * deltaTime);
+        }
+        if (moveLeftKeyDown) {
+            camera.translateX(-movementSpeed * deltaTime);
+        }
+        if (moveRightKeyDown) {
+            camera.translateX(movementSpeed * deltaTime);
+        }
 
-    var gameloop = function() {
-        requestAnimationFrame(gameloop);
-        update();
-        render();
-    };
-
-    var update = function() {
 
     };
 
@@ -103,7 +166,7 @@ var terrainSimulation = function() {
         renderer.render(scene, camera);
     };
 
-    return {init:init};
+    return {init: init};
 }();
 
 terrainSimulation.init();
