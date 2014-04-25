e2044a75903eefa54f3916f3900823499aa5fba1
PeterTheOne <petertheone@gmail.com>
2014-04-25 18:32:28 +0200

    add water

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ var terrainSimulation = function() {
     var camera;
     var renderer;
 
+    var waterGeometry;
     var geometry;
+    var waterPlane;
     var plane;
 
     var isMousedown = false;
@@ -30,6 +32,7 @@ var terrainSimulation = function() {
 
     var stone;
     var sand;
+    var water;
 
     var clock = new THREE.Clock();
 
@@ -38,7 +41,7 @@ var terrainSimulation = function() {
         camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
         camera.position.x = 0;
-        camera.position.y = 40;
+        camera.position.y = 70;
         camera.position.z = 50;
 
         renderer = new THREE.WebGLRenderer();
@@ -61,6 +64,7 @@ var terrainSimulation = function() {
 
         stone = generateStone(worldWidth, worldDepth);
         sand = generateSand(worldWidth, worldDepth);
+        water = generateWater(worldWidth, worldDepth);
         for (var i = 0; i < geometry.vertices.length; i++) {
             geometry.vertices[i].y = stone[i] + sand[i];
         }
@@ -78,6 +82,15 @@ var terrainSimulation = function() {
         var stonePlane = new THREE.Mesh(stoneGeometry, stoneMaterial);
         scene.add(stonePlane);
 
+        waterGeometry = new THREE.PlaneGeometry(geometryWidth, geometryDepth, worldWidth - 1, worldDepth - 1 );
+        waterGeometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+        waterGeometry.dynamic = true;
+        for (var i = 0; i < waterGeometry.vertices.length; i++) {
+            waterGeometry.vertices[i].y = stone[i] + sand[i] + water[i];
+        }
+        var waterMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe: true});
+        waterPlane = new THREE.Mesh(waterGeometry, waterMaterial);
+        scene.add(waterPlane);
 
         gameloop();
     };
@@ -89,10 +102,9 @@ var terrainSimulation = function() {
         for (var x = 0; x < width; x++) {
             for (var y = 0; y < height; y++) {
                 data[x * width + y] = y * 5;
-                //data[x * width + y] = 0;
+                //data[x * width + y] = 10;
             }
         }
-
         return data;
     };
 
@@ -106,7 +118,19 @@ var terrainSimulation = function() {
                 data[x * width + y] = 10;
             }
         }
+        return data;
+    };
 
+    var generateWater = function(width, height) {
+        var size = width * height;
+        var data = new Float32Array(size);
+
+        for (var x = 0; x < width; x++) {
+            for (var y = 0; y < height; y++) {
+                //data[x * width + y] = x * 2 + (y * 4) % 10;
+                data[x * width + y] = 2;
+            }
+        }
         return data;
     };
 
@@ -134,7 +158,7 @@ var terrainSimulation = function() {
                             continue;
                         }
 
-                        var heightDiff = getHeight(x1, y1) - getHeight(x, y);// sand[x * width + y] - sand[x1 * width + y1];
+                        var heightDiff = getHeight(x1, y1) - getHeight(x, y);
                         var distance = Math.sqrt(x2 * x2  + y2 * y2);
                         var slopeLength = Math.sqrt(heightDiff * heightDiff + distance * distance);
                         var alpha = Math.asin(heightDiff / slopeLength);
@@ -154,11 +178,9 @@ var terrainSimulation = function() {
         // apply diff
         for (var x = 0; x < width; x++) {
             for (var y = 0; y < height; y++) {
-                var value = sand[x * width + y] += diffData[x * width + y];
+                var value = sand[x * width + y] + diffData[x * width + y];
                 if (value < 0) {
                     sand[x * width + y] = 0;
-                //} else if (value > 255) {
-                //    sand[x * width + y] = 255;
                 } else {
                     sand[x * width + y] = value;
                 }
@@ -166,8 +188,66 @@ var terrainSimulation = function() {
         }
     };
 
+    var updateWater = function() {
+        var width = worldWidth;
+        var height = worldDepth;
+
+        var size = width * height;
+        var diffData = new Float32Array(size);
+
+        // generate diff
+        for (var x1 = 0; x1 < width; x1++) {
+            for (var y1 = 0; y1 < height; y1++) {
+                if (water[x1 * worldWidth + y1] <= 0) {
+                    continue;
+                }
+                for (var x2 = -1; x2 <= 1; x2++) {
+                    for (var y2 = -1; y2 <= 1; y2++) {
+                        var x = x1 + x2;
+                        var y = y1 + y2;
+                        if (x < 0 || x >= width || y < 0 || y >= height) {
+                            continue;
+                        }
+                        if (x2 == 0 && y2 == 0) {
+                            continue;
+                        }
+
+                        var heightDiff = getHeightWater(x1, y1) - getHeightWater(x, y);
+                        var distance = Math.sqrt(x2 * x2  + y2 * y2);
+                        var slopeLength = Math.sqrt(heightDiff * heightDiff + distance * distance);
+                        var alpha = Math.asin(heightDiff / slopeLength);
+                        // 90 degree is 1, 0 degree is 0
+                        var alphaNormalized = (alpha / Math.PI);
+                        var waterMoved = (heightDiff / 2) * 0.1;
+                        if (waterMoved > 0) {
+                            diffData[x1 * width + y1] -= waterMoved;
+                            diffData[x * width + y] += waterMoved;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        // apply diff
+        for (var x = 0; x < width; x++) {
+            for (var y = 0; y < height; y++) {
+                var value = water[x * width + y] + diffData[x * width + y];
+                if (value < 0) {
+                    water[x * width + y] = 0;
+                } else {
+                    water[x * width + y] = value;
+                }
+            }
+        }
+    };
+
     var getHeight = function(x, y) {
         return stone[x * worldWidth + y] + sand[x * worldWidth + y];
+    };
+
+    var getHeightWater = function(x, y) {
+        return stone[x * worldWidth + y] + sand[x * worldWidth + y] + water[x * worldWidth + y];
     };
 
     var resize = function() {
@@ -277,10 +357,15 @@ var terrainSimulation = function() {
         }
 
         updateSand();
+        updateWater();
         for (var i = 0; i < geometry.vertices.length; i++) {
             geometry.vertices[i].y = stone[i] + sand[i];
         }
+        for (var i = 0; i < waterGeometry.vertices.length; i++) {
+            waterGeometry.vertices[i].y = water[i] + stone[i] + sand[i];
+        }
         geometry.verticesNeedUpdate = true;
+        waterGeometry.verticesNeedUpdate = true;
     };
 
     var render = function() {
